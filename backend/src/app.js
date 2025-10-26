cacaf1fbf130ba83bd03bf665f0fd9dc9d639c4e
RahulBhardwaj-dev <Rahulbhardwaj.dev@gmail.com>
2025-10-26 18:56:30 +0530

    Made Changes for Deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from 'dotenv'
+import path from "path"
 
 import authRoutes from "./routes/authRoutes.js"
 import messageRoutes from "./routes/messageRoutes.js"
@@ -7,10 +8,20 @@ import messageRoutes from "./routes/messageRoutes.js"
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join( __dirname, "../frontend/dist")));
+
+    app.get(/.*/, (req,res) => {
+        res.sendFile(path.join( __dirname, "../frontend/dist/index.html"))
+    })
+
+}
 
 app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}`);
