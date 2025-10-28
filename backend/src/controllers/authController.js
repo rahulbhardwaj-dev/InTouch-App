d8efc62e3689c96db0b73d5c4e807a00cf52bed4
RahulBhardwaj-dev <Rahulbhardwaj.dev@gmail.com>
2025-10-28 20:50:38 +0530

    Added condition to check if user entered email and password in login controller

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -63,6 +63,11 @@ export const signup = async (req,res) => {
 
 export const login = async (req,res) => {
     let {email,password} = req.body;
+
+    if(!email || !password) {
+        return res.status(400).json({message:"Email and password are required"});
+    }
+
     try {
         const user = await User.findOne({email});//Checking if User exists
 
