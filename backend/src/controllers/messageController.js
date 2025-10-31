aac747bc446c7d55699371a6b1d78caf86cb53e9
RahulBhardwaj-dev <Rahulbhardwaj.dev@gmail.com>
2025-10-31 21:21:06 +0530

    Made Message Model and setup of MessageRoutes and messageControllers done

---
+++ backend/src/controllers/messageController.js
@@ -0,0 +1,96 @@
+import User from "../models/User.js"
+import Message from "../models/Message.js"
+import {cloudinary} from "../lib/cloudinary.js"
+
+export const getAllContacts = async (req,res) => {
+    try {
+        const loggedInUserId = req.user._id;
+        const AllUsers = await User.find({ _id : {$ne : loggedInUserId}}).select("-password") // In Contacts we want to show all users except ourselves
+
+        res.status(200).json(AllUsers)
+
+    } catch (error) {
+        console.log("Error in getAllContacts controller", error)
+        res.status(500).json({Message: "Server Error"})
+    }
+}
+
+export const getAllChats = async (req,res) => {
+    try {
+        const loggedInUserid = req.user._id;
+        
+        // First we'll fetch the messages where logged in user is sender or receiver, 
+        const messages = await Message.find({
+            $or : [{senderId: loggedInUserid}, {receiverId: loggedInUserid}]
+        });
+
+        //Getting ids of all the users which logged in user had chat with
+        //duplicate messages will be removed with the help of set, as we want to show that user once in frontend not multiple times
+        //Also converting both ids to Strings so that we can compare
+        const chatFriendIds = [
+            ...new Set
+            (messages.map((msg) => { 
+            return msg.senderId.toString() === loggedInUserid.toString()  
+            ? msg.receiverId.toString()  : msg.senderId.toString() 
+        }))]
+
+        //Getting chatFriends with extracted messages
+        const chatFriends = await User.find({_id:{$in:chatFriendIds}}).select("-password");
+        res.status(200).json(chatFriends)
+
+    } catch (error) {
+        console.log("Error in getAllChats Controller", error)
+        res.status(500).json({Message: "Server Error"})
+    }
+}
+
+export const getMessagesByUserId = async (req,res) => {
+    try {
+        const myId = req.user._id; // this id is coming from middleware, that checks if user is logged in and which user
+        const {id: userToChatId} = req.params;// this is from req, coming from our frontend action
+
+        //Finds all the messages between logged in and another user
+        const allMessages = await Message.find({
+            //filtering and getting those messages only which are between these two users
+            $or: [
+                {senderId: myId, receiverId: userToChatId},
+                {senderId: userToChatId, receiverId: myId}
+            ]
+        })
+
+        res.status(200).json(allMessages)
+
+    } catch (error) {
+        console.log("Error in getMessagesByUserId Controller", error)
+        res.status(500).json({Message: "Server Error"})
+    }
+}
+
+export const sendMessage = async (req,res) => {
+    try {
+        const {text,image} = req.body; //Message that a user have sent in chat
+        const {id: receiverId} = req.params; 
+        const senderId = req.user._id;
+
+        if(image){
+            let uploadImgMessage = await cloudinary.uploader.upload(image);
+            imageURL = uploadImgMessage.secure_url;
+        }
+
+        //Saving Message in DB
+        let newMsg = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageURL
+        })
+
+        await newMsg.save();
+
+        res.status(201).json(newMsg)
+
+    } catch (error) {
+        console.log("Error in sendMessage Controller", error)
+        res.status(500).json("Server Error");
+    }
+}
--- backend/src/routes/messageRoutes.js
+++ backend/src/routes/messageRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
+import { getAllChats, getAllContacts, getMessagesByUserId, sendMessage } from "../controllers/messageController.js";
+import { checkValid } from "../middleware/authMiddleware.js"; // Checks who is logged in and will give us req.user
+import { arcjetProtection } from "../middleware/arcjetMiddleware.js";
 
 const router = express.Router();
 
-router.get("/send", (req,res) => {
-    res.send("SENT A MESSAGE")
-})
+router.use(checkValid,arcjetProtection )
+
+router.get("/contacts",  getAllContacts); // For Contacts section
+router.get("/chats", getAllChats); //Logged in User's All chats
+router.get("/:id", getMessagesByUserId);// To get Logged in user's message
+router.post("/send/:id", sendMessage);// To send a message, /:id <- to this user
 
 export default router;
