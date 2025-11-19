8edc152ab8eeb2ef74f8d0ff557f732e3c689cdd
RahulBhardwaj-dev <Rahulbhardwaj.dev@gmail.com>
2025-11-19 11:43:24 +0530

    Final changes Done

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -94,13 +94,6 @@ export const useChatStore = create((set,get) => ({
 
       const currentMessages = get().messages;
       set({ messages: [...currentMessages, newMessage] });
-
-      if (isSoundEnabled) {
-        const notificationSound = new Audio("/sounds/notification.mp3");
-
-        notificationSound.currentTime = 0; // reset to start
-        notificationSound.play().catch((e) => console.log("Audio play failed:", e));
-      }
     });
   },
     unSubscribeFromMessages: () => {
